7b099ab9324ccbcc3822f50ec7a45621042f130c
Lysandro Carioca <lysandroc@gmail.com>
2021-10-23 19:04:18 -0300

    wip

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 set -e
 
 git clone -c core.eol=lf https://github.com/lysandroc/dotfiles ~/dotfiles
-sudo chmod 644 ~/dotfiles/setup.sh
-./dotfiles/setup.sh
+cd ~/dotfiles
+chmod 644 setup.sh
+./setup.sh
