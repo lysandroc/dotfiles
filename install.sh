d2f553b330c92a1ff3afe2841a2177f3162992d1
Lysandro Carioca <lysandroc@gmail.com>
2021-10-23 20:01:28 -0300

    wip

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p "$HOME/dotfiles"
 git clone https://github.com/lysandroc/dotfiles "$HOME/dotfiles"
-chmod 644 "$HOME/dotfiles/setup.sh"
+chmod +r+x "$HOME/dotfiles/setup.sh"
 
 echo "Installing"
 cd "$HOME/dotfiles" && bash setup.sh
