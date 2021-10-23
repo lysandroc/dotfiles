9c4f2ecd63b017d2ec51e0011f4acf07365cf675
Lysandro Carioca <lysandroc@gmail.com>
2021-10-23 12:56:58 -0300

    dotfiles directory

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 set -e
 
-git clone -c core.eol=lf https://github.com/lysandroc/dotfiles ~
+git clone -c core.eol=lf https://github.com/lysandroc/dotfiles ~/dotfiles
 chmod -x ~/dotfiles/setup.sh
 ./setup.sh
